21dbeb07edb693c5f0c87dbba17c81af85c45190
Vitaly Grishin <wildreiser@gmail.com>
2021-07-14 23:33:15 +0500

    5 stage all

--- Source/TDSKill_Game/TDSKill_GameCharacter.cpp
+++ Source/TDSKill_Game/TDSKill_GameCharacter.cpp
@@ -44,7 +44,7 @@ ATDSKill_GameCharacter::ATDSKill_GameCharacter()
 	// Create a decal in the world to show the cursor's location
 	CursorToWorld = CreateDefaultSubobject<UDecalComponent>("CursorToWorld");
 	CursorToWorld->SetupAttachment(RootComponent);
-	static ConstructorHelpers::FObjectFinder<UMaterial> DecalMaterialAsset(TEXT("Material'/Game/TopDownCPP/Blueprints/M_Cursor_Decal.M_Cursor_Decal'"));
+	static ConstructorHelpers::FObjectFinder<UMaterial> DecalMaterialAsset(TEXT("Material'/Game/Materials/M_Cursor_Decal.M_Cursor_Decal'"));
 	if (DecalMaterialAsset.Succeeded())
 	{
 		CursorToWorld->SetDecalMaterial(DecalMaterialAsset.Object);
